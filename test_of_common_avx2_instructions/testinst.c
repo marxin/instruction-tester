f71a6286eaae23cf9603180cef5e75aded32d0ba
Martin Liska <mliska@suse.cz>
2020-02-10 15:08:05 +0100

    Use macros.

--- test_of_common_avx2_instructions/testinst.c
+++ test_of_common_avx2_instructions/testinst.c
@@ -3,6 +3,9 @@
 #include <sys/time.h>
 //compilation command: gcc -march=haswell testinst.S testinst.c -o testinst
 
+const int64_t ops = 8589934592 / 2; // 2^33 operations per test
+const float frequency = 4.3f;
+
 extern void test_latency_vpermpd(int64_t ops);
 extern void test_latency_vblendpd(int64_t ops);
 extern void test_latency_vperm2f128(int64_t ops);
@@ -20,122 +23,43 @@ extern void test_throughput_vaddpd(int64_t ops);
 extern void test_throughput_vmulpd(int64_t ops);
 extern void test_throughput_vhaddpd(int64_t ops);
 
+#define TEST_LATENCY(insn) \
+  gettimeofday(&starttime,0); \
+  test_latency_ ## insn(ops); \
+  gettimeofday(&endtime,0); \
+  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec; \
+  gops_per_sec = (double)ops / usec_elapsed / 1000; \
+  printf("GOPs per second for " # insn " chained instructions: %e, latency: %.2f\n", gops_per_sec, frequency / gops_per_sec);
+
+#define TEST_THROUGHPUT(insn) \
+  gettimeofday(&starttime,0); \
+  test_throughput_ ## insn(ops); \
+  gettimeofday(&endtime,0); \
+  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec; \
+  gops_per_sec = (double)ops / usec_elapsed / 1000; \
+  printf("GOPs per second for " # insn " independent instructions: %e, IPC: %.2f\n", gops_per_sec, frequency / gops_per_sec);
+
+#define TEST(insn) \
+{ \
+  TEST_THROUGHPUT(insn); \
+  TEST_LATENCY(insn); \
+  printf("\n"); \
+}
+  
+
 int main(){
+  printf ("CPU frequency: %.2f GHz\n", frequency);
   struct timeval starttime,endtime;
   double usec_elapsed, gops_per_sec;
-  const int64_t ops = 8589934592; // 2^33 operations per test
 
-  gettimeofday(&starttime,0);
-  test_latency_vpermpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vpermpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_latency_vblendpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vblendpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_latency_vperm2f128(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vperm2f128 instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_latency_vshufpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vshufpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_latency_vfmadd231pd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vfmadd231pd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_latency_vaddpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vaddpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_latency_vmulpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vmulpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_latency_vhaddpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for chained vhaddpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vpermpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vpermpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vblendpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vblendpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vperm2f128(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vperm2f128 instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vshufpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vshufpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vfmadd231pd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vfmadd231pd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vaddpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vaddpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vmulpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vmulpd instructions: %e\n", gops_per_sec);
-
-  gettimeofday(&starttime,0);
-  test_throughput_vhaddpd(ops);
-  gettimeofday(&endtime,0);
-  usec_elapsed = 1000000*(endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
-  gops_per_sec = (double)ops / usec_elapsed / 1000;
-  printf("GOPs per second for independent vhaddpd instructions: %e\n", gops_per_sec);
+  TEST(vpermpd);
+  TEST(vblendpd);
+  TEST(vperm2f128);
+  TEST(vshufpd);
+  TEST(vfmadd231pd);
+  TEST(vaddpd);
+  TEST(vmulpd);
+  TEST(vhaddpd);
 
   return 0;
 }
